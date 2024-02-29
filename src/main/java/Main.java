e0ef04fc4b242efe0a3ac7b28b6e1ad05c9bde37
AliceGonzalez <alicegcodes@gmail.com>
2024-02-29 14:04:09 -0500

    Added Classes

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -2,22 +2,26 @@
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
-public class Main {
 
+
+public class Main {
+    public String name;
     public static void main(String[] args ){
         // create a new object of class Main
-
+         Main user1 = new Main(this.name);
         // call the compute() method on that new object
+        System.out.println(user1.compute());
     }
 
-    public void compute() {
+    public boolean compute() {
         // Get input string of name from user
-        
+
         // Create conditional to check name against Alice and Bob
 
         // Print greeting to screen if Alice or Bob are true
         
         // Print "You are neither Alice nor Bob." otherwise
 
+        return false;
     }
 }
