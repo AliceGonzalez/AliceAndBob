ee5d0af1a984d78575ff7da6740abe754e3a000d
AliceGonzalez <alicegcodes@gmail.com>
2024-02-29 19:05:24 -0500

    Finished

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,27 +1,32 @@
+import java.util.Scanner;
+
 /**
  * Created by iyasuwatts on 10/17/17.
  * Modified by kristofer on Jan 2024.
  */
 
-
 public class Main {
-    public String name;
+
     public static void main(String[] args ){
-        // create a new object of class Main
-         Main user1 = new Main(this.name);
-        // call the compute() method on that new object
-        System.out.println(user1.compute());
+
+        // call the compute() method
+        compute();
     }
 
-    public boolean compute() {
-        // Get input string of name from user
+    public static boolean compute() {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter your name: ");
+        String name = scanner.nextLine();
+
 
         // Create conditional to check name against Alice and Bob
-
+        if (name.equals("alice") || name.equals("bob")) {
         // Print greeting to screen if Alice or Bob are true
-        
+            System.out.println("Hello " + name + "!");
         // Print "You are neither Alice nor Bob." otherwise
-
+        } else {
+        System.out.println("You are neither Alice nor Bob.");
+        }
         return false;
     }
 }
